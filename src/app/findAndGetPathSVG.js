a2870982a0f691e6f4087c35d07fac941117d704
pyplee <dekimiq@gmail.com>
2024-06-15 19:43:43 +0300

    added func for constract path to svg

---
+++ src/app/findAndGetPathSVG.js
@@ -0,0 +1,11 @@
+const convertNameToNameSVG = (nameSVG) => {
+  const tech = new String(nameSVG);
+  const lowerCase = tech.toLowerCase();
+  const splited = lowerCase.split(' ');
+  const pathToDirectorySVGS = '/technologies_logo/';
+  const fotmatFile = '.svg';
+  const svgName = splited.join('-');
+  return `${pathToDirectorySVGS}${svgName}${fotmatFile}`;
+};
+
+export default convertNameToNameSVG;
