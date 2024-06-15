ca37a9dceec31e4a175f7c01d1be29d7add2785e
pyplee <dekimiq@gmail.com>
2024-06-15 16:43:04 +0300

    added page auth

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,67 @@
+"use client";
+import React from "react";
+// Define the form schema using Zod
+
+
+function MainComponent() {
+  return (
+    <div className="min-h-screen flex">
+      <div className="flex-1 bg-[#2e3138] flex flex-col items-start p-4">
+        {/* <img
+          src="/path-to-logo.png"
+          alt="GSfind logo"
+          className="h-[40px] mb-4"
+        />
+        <img
+          src="/path-to-your-image.jpg"
+          alt="abstract background"
+          className="flex-grow object-cover"
+        /> */}
+      </div>
+      <div className="flex-1 bg-[#383b42] p-8 flex flex-col items-center justify-center">
+        <h2 className="text-white font-bold text-2xl mb-6">Вход / регистрация</h2>
+        <p className="text-white mb-4">
+          Почему нет формы{" "}
+          <a href="#" className="text-[#ff8300]">
+            Входа / регистрации ?
+          </a>
+        </p>
+        <div className="w-1/2 space-y-4">
+        <button className="w-full bg-white text-black flex items-center justify-center py-2 rounded-md">
+            <img
+              className="fab fa-github mr-2"
+              src="/google.svg"
+              alt="Github logo"
+              width="30px"
+              height="30px"
+            />
+            Войти через google
+          </button>
+          <button className="w-full bg-white text-black flex items-center justify-center py-2 rounded-md">
+            <img
+              className="fab fa-github mr-2"
+              src="/github_logo.svg"
+              alt="Github logo"
+              width="30px"
+              height="30px"
+            />
+            Войти через github
+          </button>
+          <button className="w-full bg-[#ff8300] text-white flex items-center justify-center py-2 rounded-md">
+            <img
+              className="fab fa-github mr-2"
+              src="/telegram.svg"
+              alt="Github logo"
+              width="35px"
+              height="35px"
+            />
+            Войти через telegram
+          </button>
+          {/* <div className="bg-gray-400 h-1 w-full rounded-full"></div> */}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default MainComponent;
---
+++ src/app/auth/progressive-auth/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+import React from "react";
+// Define the form schema using Zod
+
+
+function MainComponent() {
+  return (
+    <div className="min-h-screen flex">
+      <h1>TEST</h1>
+    </div>
+  );
+}
+
+export default MainComponent;
