137013924dcee19c6a441762482759afe1342aa8
pyplee <dekimiq@gmail.com>
2024-06-15 16:49:09 +0300

    added padding on logo

--- src/app/ui/footer.tsx
+++ src/app/ui/footer.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 function Footer() {
   return (
-  <footer className="bg-[#222831] text-center p-2 flex justify-end items-center space-x-2">
+  <footer className="bg-[#222831] text-center p-2 flex justify-end items-center space-x-2 px-20">
     <Link href="https://github.com" className="text-[#DC5F00] text-2xl">
       <Image src="/github_logo.svg"
         alt="Github logo"
