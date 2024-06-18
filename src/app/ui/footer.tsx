37c168fd500d01e3e88d04351b0b2f3703a78f44
pyplee <dekimiq@gmail.com>
2024-06-18 19:16:36 +0300

    added class for padding

--- src/app/ui/footer.tsx
+++ src/app/ui/footer.tsx
@@ -3,15 +3,17 @@ import Link from 'next/link'
 
 function Footer() {
   return (
-  <footer className="bg-[#222831] text-center p-2 flex justify-end items-center space-x-2 px-20">
-    <Link href="https://github.com" className="text-[#DC5F00] text-2xl">
-      <Image src="/github_logo_orange.svg"
-        alt="Github logo"
-        width={35}
-        height={35}
-        />
-    </Link>
-  </footer>
+  <div className='bg-[#EEEEEE] pt-20'>
+    <footer className="bg-[#222831] text-center p-2 flex justify-end items-center space-x-2 px-20">
+      <Link href="https://github.com" className="text-[#DC5F00] text-2xl">
+        <Image src="/github_logo_orange.svg"
+          alt="Github logo"
+          width={35}
+          height={35}
+          />
+      </Link>
+    </footer>
+  </div>
   );
 }
 
